0c51cb80168424d0987cb692e6dc381c6d5a14fc
Rezwan Ahmed <rezwan.ahmed@ibcs-primax.com>
2025-06-08 22:39:06 +0600

    Author entity created

---
+++ src/main/java/com/rezocoding/jpa/entites/Author.java
@@ -0,0 +1,45 @@
+package com.rezocoding.jpa.entites;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "AUTHOR_TBL")
+public class Author {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(
+            name = "f_name",
+            length = 35
+    )
+    private String firstName;
+
+    @Column(
+            name = "l_name",
+            length = 35
+    )
+    private String lastName;
+
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private String email;
+
+    private int age;
+
+    @Column(name = "created_at", updatable = false,nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at",insertable = false)
+    private LocalDateTime updatedAt;
+
+}
